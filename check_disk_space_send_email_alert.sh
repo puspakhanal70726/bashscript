63683c1d31952fd3a2a4ee1073ce7835ed666297
Administrator <khanalpuspa70726@gmail.com>
2019-09-25 17:17:25 -0400

    added two more files

---
+++ check_disk_space_send_email_alert.sh
@@ -0,0 +1,7 @@
+MAX=20
+EMAIL=khanalpuspa70726@gmail.com
+PART=sda1
+USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
+if [ $USE -gt $MAX ]; then
+  echo "Percent used: $USE" | mail -s "Running out of disk space" $EMAIL
+fi
